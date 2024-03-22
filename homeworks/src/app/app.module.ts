6dd0ff39d25c25b0b9b2dfab8a175cadcbd8f959
Mario Barajas <109113229+Mario-Barajas@users.noreply.github.com>
2024-03-22 17:06:59 -0600

    Homework-1: NgClass done

--- homeworks/src/app/app.module.ts
+++ homeworks/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Homework1Component } from './components/homework1/homework1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Homework1Component
   ],
   imports: [
     BrowserModule,
