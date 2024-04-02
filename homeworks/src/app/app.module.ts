8a93fc07efdec9a9fbb48d6ed8b68f1285bf7ce1
Mario Barajas <109113229+Mario-Barajas@users.noreply.github.com>
2024-04-02 10:39:54 -0600

    homework-5: component and module created

--- homeworks/src/app/app.module.ts
+++ homeworks/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Homework5Component } from './components/homework5/homework5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Homework5Component
   ],
   imports: [
     BrowserModule,
---
+++ homeworks/src/app/components/homework5/homework5.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homework5',
+  templateUrl: './homework5.component.html',
+  styleUrl: './homework5.component.scss'
+})
+export class Homework5Component {
+
+}
