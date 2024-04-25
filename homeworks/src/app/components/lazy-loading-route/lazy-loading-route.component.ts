90944e0e57d320583df3e9bbc0487e92959ba75b
Mario Barajas <109113229+Mario-Barajas@users.noreply.github.com>
2024-04-25 11:59:06 -0600

    homework-9: lazy loading module done

---
+++ homeworks/src/app/components/lazy-loading-route/lazy-loading-route.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lazy-loading-route',
+  templateUrl: './lazy-loading-route.component.html',
+  styleUrl: './lazy-loading-route.component.scss',
+})
+export class LazyLoadingRouteComponent {}
