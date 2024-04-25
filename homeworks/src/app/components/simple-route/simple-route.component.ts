ab08af060a4c576a58200647585b1a180e21c15d
Mario Barajas <109113229+Mario-Barajas@users.noreply.github.com>
2024-04-25 11:58:10 -0600

    homework-9: simple route done

---
+++ homeworks/src/app/components/simple-route/simple-route.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'simple-route',
+  templateUrl: './simple-route.component.html',
+  styleUrl: './simple-route.component.scss',
+})
+export class SimpleRouteComponent {}
