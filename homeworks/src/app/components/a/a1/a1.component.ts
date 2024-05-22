4f792277cfc607345312e2fd16d9e7860532f155
Mario Barajas <109113229+Mario-Barajas@users.noreply.github.com>
2024-05-21 18:40:48 -0600

    A component done

---
+++ homeworks/src/app/components/a/a1/a1.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-a1',
+  templateUrl: './a1.component.html',
+  styleUrl: './a1.component.scss',
+})
+export class A1Component {}
